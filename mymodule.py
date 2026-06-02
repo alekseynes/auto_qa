e1655e6cd0c1988d4e4402334448aa466cf6cb6c
alexeynes <anesterenko880@gmail.com>
2026-06-02 23:16:02 +0300

    функция кофе

--- mymodule.py
+++ mymodule.py
@@ -1,10 +1,20 @@
-name = "Aleksey"
+# name = "Aleksey"
+#
+# def hello():
+#     print("Hello", name)
+#
+# def add_three_numbers(a, b, c):
+#     if a != 0 and b != 0 and c != 0:
+#         return a + b + c
+#     else:
+#         return "some is zeroФ"
 
-def hello():
-    print("Hello", name)
+def make_coffee():
+    print("Делаю кофе")
+    print("1. Грею воду...")
+    print("2. Добавляю кофейные зерна...")
+    print("3. Добавляю сахар...")
+    print("4. Добавляю молоко...")
+    print("Кофе готов!")
 
-def add_three_numbers(a, b, c):
-    if a != 0 and b != 0 and c != 0:
-        return a + b + c
-    else:
-        return "some is zero"
+make_coffee()
